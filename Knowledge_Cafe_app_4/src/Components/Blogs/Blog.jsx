5f67a4629f4fa9785e0733bf685640b99ae31b0a
Mohammad-Abdur-Rahim <abdur.rahim2992001@gmail.com>
2025-02-02 21:38:04 +0600

    ** Load all data and design implememnt**

--- Knowledge_Cafe_app_4/src/Components/Blogs/Blog.jsx
+++ Knowledge_Cafe_app_4/src/Components/Blogs/Blog.jsx
@@ -1,13 +1,32 @@
 import PropTypes from 'prop-types';
-const Blog = ({blog}) => {
-    console.log(blog);
+const Blog = ({ blog }) => {
+    const { title, cover, reading_time, author, author_img,posted_date,hashtag } = blog;
     return (
         <div>
-            
+            <img className='w-4xl ml-8' src={cover} alt="nothing img" />
+            <div className='flex justify-between items-center ml-8 '>
+                <div className='flex pt-5'>
+                    <img className='w-14 rounded-full' src={author_img} alt="" />
+                <div className='ml-8'>
+                    <h3 className='text-2xl'>{author}</h3>
+                    <p>{posted_date}</p>
+                </div>
+                </div>
+                <div>
+        <p>{reading_time} min read</p>
+                </div>
+            </div>
+            <h2 className='text-4xl ml-8 p-5'>{title}</h2>
+            <p>
+                {
+                    hashtag.map((hash,idx) => <span key={idx}><a  href="">{hash}</a></span> )
+                }
+            </p>
+
         </div>
     );
 };
-Blog.propTypes ={
+Blog.propTypes = {
     blog: PropTypes.object.isRequired
 }
 export default Blog;
--- Knowledge_Cafe_app_4/src/Components/Blogs/Blogs.jsx
+++ Knowledge_Cafe_app_4/src/Components/Blogs/Blogs.jsx
@@ -1,23 +1,30 @@
 import { useEffect } from "react";
 import { useState } from "react";
+import Blog from "./Blog";
 
 const Blogs = () => {
 
 
-const [blogs,setBlogs] = useState([]);
+    const [blogs, setBlogs] = useState([]);
 
-useEffect(()=>{
-    fetch('blogs.json')
-    .then(res=>res.json())
-    .then(data=>setBlogs(data))
-},[])
-
-
+    useEffect(() => {
+        fetch('blogs.json')
+            .then(res => res.json())
+            .then(data => setBlogs(data))
+    }, [])
 
 
     return (
         <div className="md:w-2/3">
             <h2 className="text-2xl">Blogs :{blogs.length} </h2>
+            {
+                blogs.map(blog =>
+                    <Blog
+                        key={blog.id}
+                        blog={blog}>
+
+                    </Blog>)
+            }
         </div>
     );
 };
