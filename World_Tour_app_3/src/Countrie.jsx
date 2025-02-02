db8ff66da0a1f6aa2fb510e9ba318cf2a9de5690
Mohammad-Abdur-Rahim <abdur.rahim2992001@gmail.com>
2025-02-02 12:16:12 +0600

    * M-40 Done *

--- World_Tour_app_3/src/Countrie.jsx
+++ World_Tour_app_3/src/Countrie.jsx
@@ -1,13 +1,13 @@
 import { useState } from 'react';
 import './Country.css';
-const Countrie = ({countrie}) => {
+const Countrie = ({countrie,handleMark,visitedFlag}) => {
    
     const {name,flags,population} = countrie;
     const [visited,setVisited] = useState(false);
     const handleVisited =()=>{
         setVisited(!visited);
     }
-    visited
+
     return (
         <div className={`country ${visited && 'visited'}`}>
             <h1>{name.common} </h1>
@@ -15,7 +15,9 @@ const Countrie = ({countrie}) => {
             <p>Population : {population}</p>
             <button onClick={handleVisited} style={{ marginRight: "10px" }}>{visited?'visited':'Going'}</button>
             {visited ? 'already visited':'not visited'}
-            <button style={{ marginLeft: "10px" }}>Mark</button>
+            <button onClick={()=>handleMark(countrie)} style={{ marginLeft: "10px" }}>Mark</button>
+            <br />
+            <button onClick={()=>visitedFlag(countrie.flags.png)}>Add Flag</button>
 
         </div>
     );
--- World_Tour_app_3/src/Countries.jsx
+++ World_Tour_app_3/src/Countries.jsx
@@ -6,28 +6,50 @@ import './Container.css';
 const Countries = () => {
     const [countries, setCountries] = useState([]);
 
-    useEffect(() => {
+    const [flags , setFlag] = useState([]);
+    const visitedFlag =(flag)=>{
+        const newFlag = [... flags, flag]
+        setFlag(newFlag);
+    };
 
+    const [mark,setMark] = useState([]);
+    const handleMark =(country) => {
+        const newMark =[...mark,country];
+        setMark(newMark);
+    }
+    ;
+    useEffect(() => {
         fetch('https://restcountries.com/v3.1/all')
             .then(res => res.json())
             .then(data => setCountries(data));
     }, [])
 
-
     return (
         <div>
             <h3>Country : {countries.length}</h3>
+
+             {/* Visited country  */}
+           
+              
           <div>
-          <h5>Visited Country :</h5>
+          <h5>Visited Country :{mark.length}</h5>
           <ul>
-            
+        {
+            mark.map(country => <li key={countries.cca3}>{country.name.common}</li> )
+        }
           </ul>
           </div>
-       
+          <div className="flag_container">
+          {
+              flags.map((flag,idx) => <img key={idx} src={flag} /> )
+          }
+       </div>
+       {/* display country  */}
         <div className="container">
             {
-                countries.map(c => 
-                <Countrie key={countries.cca3} countrie={c}></Countrie> 
+                countries.map((c,idx) => 
+                <Countrie handleMark={handleMark} visitedFlag={visitedFlag}
+                 key={idx} countrie={c}></Countrie> 
             )
             }
         </div>
